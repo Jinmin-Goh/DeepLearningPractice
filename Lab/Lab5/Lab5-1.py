05a12e03d453cccf6c855f55483c1304bd2dfa06
Jinmin Goh <eric970901@gmail.com>
2020-03-08 02:29:16 +0900

    Lab 5 source code

---
+++ Lab/Lab5/Lab5-1.py
@@ -0,0 +1,57 @@
+# Lab 5-1
+# Made by:  Jinmin Goh
+# Date:     20200218
+
+# Logistic Regression Classifier
+
+import tensorflow as tf
+tf.set_random_seed(777)  # for reproducibility
+
+# dataset, x: multi-variable / y: 0 or 1
+x_data = [[1, 2],
+          [2, 3],
+          [3, 1],
+          [4, 3],
+          [5, 3],
+          [6, 2]]
+y_data = [[0],
+          [0],
+          [0],
+          [1],
+          [1],
+          [1]]
+
+# placeholders for a tensor that will be always fed.
+X = tf.placeholder(tf.float32, shape=[None, 2])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random_normal([2, 1]), name='weight')
+    # [2, 1] means 2 values are input(x_data) & 1 value is output(y_data)
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# cost/loss function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+
+# We don't need derivative; TF will do it
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+    # cast float to bool
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# Launch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+    for step in range(10001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+        if step % 200 == 0:
+            print(step, cost_val)
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy],
+                       feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a)
